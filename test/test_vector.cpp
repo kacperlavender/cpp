c53a8a30f35aad38dfb79fd9edafeb305be5e9d1
Kacper Kiełbasa <kacper@MacBook-Air-Kacper.local>
2025-04-05 23:14:51 +0200

    matlab2

--- test/test_vector.cpp
+++ test/test_vector.cpp
@@ -1,15 +1,65 @@
 #include "gtest/gtest.h"
 #include "matlab.hpp"
 
-TEST(MatlabVectorTest, add) {
-    const std::size_t v_size = 3;
+TEST(MatlabVectorTest, VectorAddition) {
+    const std::size_t size = 3;
     int v1[] = {1, 2, 3};
     int v2[] = {4, 5, 6};
 
-    int* v_sum = add_vectors(v1, v2, v_size);
-    ASSERT_NE((int*) NULL, v_sum);
+    int* result = add_vectors(v1, v2, size);
+    ASSERT_NE(result, nullptr);
 
-    EXPECT_EQ(v_sum[0], 5);
-    EXPECT_EQ(v_sum[1], 7);
-    EXPECT_EQ(v_sum[2], 9);
+    EXPECT_EQ(result[0], 5);
+    EXPECT_EQ(result[1], 7);
+    EXPECT_EQ(result[2], 9);
+
+    delete[] result;
+}
+
+TEST(MatlabVectorTest, EmptyVectorAddition) {
+    int* result = add_vectors(nullptr, nullptr, 0);
+    EXPECT_EQ(result, nullptr);
+}
+
+TEST(MatVectClassTest, ConstructorAndSize) {
+    const std::size_t size = 5;
+    MatVect vec(size);
+
+    EXPECT_EQ(vec.size(), size);
+}
+
+TEST(MatVectClassTest, ElementAccess) {
+    MatVect vec(3);
+    vec.set_elem(0, 10);
+    vec.set_elem(1, 20);
+    vec.set_elem(2, 30);
+
+    EXPECT_EQ(vec.get_elem(0), 10);
+    EXPECT_EQ(vec.get_elem(1), 20);
+    EXPECT_EQ(vec.get_elem(2), 30);
+}
+
+TEST(MatVectClassTest, NormCalculation) {
+    MatVect vec(3);
+    vec.set_elem(0, 3);
+    vec.set_elem(1, 4);
+    vec.set_elem(2, 0);
+
+    EXPECT_DOUBLE_EQ(vec.norm(), 5.0);
+}
+
+TEST(MatVectClassTest, BoundaryCheck) {
+    MatVect vec(3);
+
+    EXPECT_THROW(vec.get_elem(3), std::out_of_range);
+    EXPECT_THROW(vec.set_elem(3, 0), std::out_of_range);
+}
+
+TEST(PrintTest, VectorPrinting) {
+    int v[] = {1, 2, 3};
+    testing::internal::CaptureStdout();
+    print_vector(v, 3);
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "1 2 3 ");
 }
